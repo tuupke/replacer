eb2781f43a9ca540e7aaaee260783ea71116be92
Mart Pluijmaekers <mart@tuupke.nl>
2018-10-03 18:06:04 +0200

    Implemented a basic clean method, only removes pidfiles currently

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"strings"
 	"regexp"
+	"path/filepath"
 )
 
 func main() {
@@ -29,7 +30,7 @@ func main() {
 
 		parsed := regexp.MustCompile("\\${[^}]+}").ReplaceAllFunc(fileBytes, func(bytes []byte) []byte {
 			// Replace with lookup in environment file
-			return []byte(env[string(bytes[2:len(bytes) - 1])])
+			return []byte(env[string(bytes[2:len(bytes)-1])])
 		})
 
 		fmt.Println("Generated configfile\n", string(parsed))
@@ -48,7 +49,7 @@ func main() {
 		signal.Notify(sigs, syscall.SIGABRT, syscall.SIGALRM, syscall.SIGBUS, syscall.SIGCHLD, syscall.SIGCONT, syscall.SIGFPE, syscall.SIGHUP, syscall.SIGILL, syscall.SIGINT, syscall.SIGIO, syscall.SIGIOT, syscall.SIGKILL, syscall.SIGPIPE, syscall.SIGPROF, syscall.SIGQUIT, syscall.SIGSEGV, syscall.SIGSTOP, syscall.SIGSYS, syscall.SIGTERM, syscall.SIGTRAP, syscall.SIGTSTP, syscall.SIGTTIN, syscall.SIGTTOU, syscall.SIGURG, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGVTALRM, syscall.SIGWINCH, syscall.SIGXCPU, syscall.SIGXFSZ)
 
 		go func() {
-			defer func() {done <- true}()
+			defer func() { done <- true }()
 
 			if len(flag.Args()) <= 2 {
 				return
@@ -59,7 +60,7 @@ func main() {
 			cmd := exec.Command(rest[0], rest[1:]...)
 			if cmd == nil {
 				fmt.Println("Error starting command", rest[0])
-				os.Exit(41)
+				cleanAndExit(41)
 			}
 
 			cmd.Stderr = os.Stderr
@@ -67,10 +68,10 @@ func main() {
 
 			go func() {
 				for true {
-					s := <- sigs
+					s := <-sigs
 
 					if cmd.Process == nil {
-						os.Exit(42)
+						cleanAndExit(42)
 					}
 
 					fmt.Println("About to send singnal", s.String())
@@ -84,14 +85,31 @@ func main() {
 			if err, ok := err.(*exec.ExitError); ok {
 				if status, ok := err.Sys().(syscall.WaitStatus); ok {
 					fmt.Printf("Exit Status: %d\n", status.ExitStatus())
-					os.Exit(status.ExitStatus())
+					cleanAndExit(status.ExitStatus())
 				}
 			}
 		}()
 
 		// Await finishing of command
-		<- done
+		<-done
 	} else {
 		fmt.Println("Could not open basefile for parsing", err)
 	}
+
+	cleanAndExit(0)
+}
+
+func cleanAndExit(exitCode int) {
+	files, err := filepath.Glob("*.pid")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		for _, f := range files {
+			if err := os.Remove(f); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+	os.Exit(exitCode)
 }
